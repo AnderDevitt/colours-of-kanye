8672fa311be9820947f252aea0bf207f79425194
Cameron Jones <iamcrjones@gmail.com>
2022-04-28 15:38:58 +1000

    kaney api now alters quote div text

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // API FETCH REQUESTS HERE
+
+// Kanye Quotes
+const card1 = document.getElementsByClassName("quotesCardQuote1")
+const card2 = document.getElementsByClassName("quotesCardQuote2")
+const card3 = document.getElementsByClassName("quotesCardQuote3")
+const card4 = document.getElementsByClassName("quotesCardQuote4")
+const hero = document.getElementsByClassName("quotesHeroQuote")
 async function getKanye() {
-    let response = await fetch("https://api.kanye.rest")
-    let data = await response.json()
-    console.log(data.quote)
-    printKanye(data.quote)
+    let cardArray = []
+    let quoteArray = []
+    cardArray.push(card1.item(0), card2.item(0), card3.item(0), card4.item(0), hero.item(0))
+    console.log(cardArray)
+    for(let i = 0; i < cardArray.length; i++) {
+        let response = await fetch("https://api.kanye.rest")
+        let data = await response.json()
+        console.log(data.quote)
+        quoteArray.push(data.quote)
+    }
+    for(let i = 0; i < cardArray.length; i++){
+        cardArray[i].innerText = quoteArray[i]
+    }
 }
-const domBody = document.querySelector('body')
-
-function printKanye(kanye) {
-    let divKanye = document.createElement("div")
-    divKanye.setAttribute("id", "divKanye")
-    let kanyeH1 = document.createElement("h1")
-    kanyeH1.innerText = kanye
-    divKanye.appendChild(kanyeH1)
-    domBody.appendChild(divKanye)
-}
-
 getKanye();
+
